42df2b46fe21f47b17965f707e4eb15fe7a69c5e
Julie-Anne Roder <julieanne.roder@my.jcu.edu.au>
2020-08-29 08:25:24 +1000

    Initial commit

---
+++ Assignment 1/travel_tracker.py
@@ -0,0 +1,14 @@
+"""
+CP1404 Assignment 1 - Travel Tracker 1.0
+Name: Julie-Anne Roder
+Date started:29/08/2020
+GitHub URL:
+"""
+
+
+def main():
+    print("Travel Tracker 1.0 - by Julie-Anne Roder")
+
+
+if __name__ == '__main__':
+    main()
