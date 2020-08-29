ceb07cbd8372ba8c84718ecb47592bfc0b97c06c
Julie-Anne Roder <julieanne.roder@my.jcu.edu.au>
2020-08-29 11:14:42 +1000

    Add display places function

--- Assignment 1/travel_tracker.py
+++ Assignment 1/travel_tracker.py
@@ -6,6 +6,7 @@ GitHub URL: https://github.com/JulieRoder/Assignments/tree/master/Assignment%201
 """
 HIGH_PRIORITY = 1
 LOW_PRIORITY = 12
+PRIORITY_LENGTH = 3
 MENU = "Menu:\nL - List places\nA - Add new place\nM - Mark a place as visited\nQ - Quit"
 FILENAME = "places.csv"
 
@@ -19,11 +20,13 @@ def main():
     menu_choice = get_valid_menu_choice(menu_options)
     while menu_choice != "Q":
         if menu_choice == "L":
-            pass
+            display_places_list(places)
         elif menu_choice == "A":
             pass
         else:
             pass
+        print(MENU)
+        menu_choice = get_valid_menu_choice(menu_options)
 
 
 def get_places_data():
@@ -40,12 +43,37 @@ def get_places_data():
 
 
 def get_valid_menu_choice(menu):
-    """Get a valid menu choice"""
+    """Get a valid menu choice."""
     choice = input(">>> ").upper()
     while choice not in menu:
-        print("Invalid option")
+        print("Invalid menu choice")
         choice = input(">>> ").upper()
     return choice
+
+
+def display_places_list(places):
+    """Display list of places."""
+    index = 0
+    longest_place_name = get_longest_name(places, 0)
+    longest_country_name = get_longest_name(places, 1)
+    places.sort()
+    for place in places:
+        index += 1
+        if place[3] == "n":
+            print("*{}. {:<{}} in {:<{}} priority {:>{}}".format(index, place[0], longest_place_name, place[1],
+                                                                 longest_country_name, place[2], PRIORITY_LENGTH))
+        else:
+            print(" {}. {:<{}} in {:<{}} priority {:>{}}".format(index, place[0], longest_place_name, place[1],
+                                                                 longest_country_name, place[2], PRIORITY_LENGTH))
+
+
+def get_longest_name(list_file, index):
+    """Get length of longest name."""
+    longest_name = 0
+    for parts in list_file:
+        if len(parts[index]) > longest_name:
+            longest_name = len(parts[index])
+    return longest_name
 
 
 if __name__ == '__main__':
