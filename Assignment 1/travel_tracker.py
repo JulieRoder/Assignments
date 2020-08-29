e2d02a1ff49fbb2f5a77cc329db865d3fd3db38d
Julie-Anne Roder <julieanne.roder@my.jcu.edu.au>
2020-08-29 15:34:37 +1000

    Add more constants

--- Assignment 1/travel_tracker.py
+++ Assignment 1/travel_tracker.py
@@ -8,11 +8,14 @@ from operator import itemgetter
 FILENAME = "places.csv"
 MENU = "Menu:\nL - List places\nA - Add new place\nM - Mark a place as visited\nQ - Quit"
 SENTINEL = 1
+STARTING_VALUE = 0
 VISITED_INDEX = 3
 PRIORITY_INDEX = 2
 COUNTRY_INDEX = 1
 PLACE_NAME_INDEX = 0
 PRIORITY_LENGTH = 3
+UNVISITED_MARKER = "n"
+VISITED_MARKER = "v"
 
 
 def main():
@@ -31,10 +34,16 @@ def main():
             places.append(place_details)
             places.sort(key=itemgetter(VISITED_INDEX, PRIORITY_INDEX))
         else:
-            display_places_list(places)
+            visited_count = STARTING_VALUE
+            for place in places:
+                if place[VISITED_INDEX] == VISITED_MARKER:
+                    visited_count += 1
+            if visited_count == len(places):
+                print("No unvisited places")
+            else:
+                display_places_list(places)
 
-            places.sort(key=itemgetter(VISITED_INDEX, PRIORITY_INDEX))
-            pass
+                places.sort(key=itemgetter(VISITED_INDEX, PRIORITY_INDEX))
         print(MENU)
         menu_choice = get_valid_menu_choice(menu_options)
 
@@ -63,13 +72,13 @@ def get_valid_menu_choice(menu):
 
 def display_places_list(places):
     """Display list of places."""
-    index = 0
+    index = STARTING_VALUE
     longest_place_name = get_longest_name(places, PLACE_NAME_INDEX)
     longest_country_name = get_longest_name(places, COUNTRY_INDEX)
     for place in places:
         index += 1
-        visit_marker = "*" if place[VISITED_INDEX] == "n" else " "
-        print("{}{}. {:<{}} in {:<{}} priority {:>{}}".format(visit_marker, index, place[PLACE_NAME_INDEX],
+        unvisited_marker = "*" if place[VISITED_INDEX] == UNVISITED_MARKER else " "
+        print("{}{}. {:<{}} in {:<{}} priority {:>{}}".format(unvisited_marker, index, place[PLACE_NAME_INDEX],
                                                               longest_place_name, place[COUNTRY_INDEX],
                                                               longest_country_name, place[PRIORITY_INDEX],
                                                               PRIORITY_LENGTH))
@@ -77,7 +86,7 @@ def display_places_list(places):
 
 def get_longest_name(list_file, index):
     """Get length of longest name."""
-    longest_name = 0
+    longest_name = STARTING_VALUE
     for parts in list_file:
         if len(parts[index]) > longest_name:
             longest_name = len(parts[index])
@@ -118,6 +127,7 @@ def collect_place_details():
     priority_value = get_valid_number("Priority: ")
     place_details.append(priority_value)
     place_details.append("n")
+    print("{} in {} (priority {}) added to Travel Tracker".format(place_name, country_name, priority_value))
     return place_details
 
 
